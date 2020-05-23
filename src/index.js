d6a76cdd424531b4b27eb0a08e933cdf7475cd14
Le Duy Anh <ledzanh@gmail.com>
2020-05-24 00:38:36 +0700

    :fire: Remove unused dependencies

--- src/index.js
+++ src/index.js
@@ -1,48 +1,42 @@
 /* @flow */
 
 import he from 'he';
-import axios from 'axios';
-import { find } from 'lodash';
 import striptags from 'striptags';
 import { JSDOM } from 'jsdom';
+import fetch from 'node-fetch';
 
-export async function getSubtitles({
-  videoID,
-  lang = 'en',
-}: {
+type SubtitleOption = {
   videoID: string,
   lang: 'en' | 'de' | 'fr' | void,
-}) {
+};
+
+export async function getSubtitles({ videoID, lang = 'en' }: SubtitleOption) {
   const dom = await JSDOM.fromURL(
     `https://youtube.com/watch?v=${videoID}&persist_app=1&app=m`
   );
 
-  const decodedData = dom.serialize();
+  const serializedDom = dom.serialize();
 
   // * ensure we have access to captions data
-  if (!decodedData.includes('captionTracks'))
+  if (!serializedDom.includes('captionTracks'))
     throw new Error(`Could not find captions for video: ${videoID}`);
 
   const regex = /({"captionTracks":.*isTranslatable":(true|false)}])/;
-  const [match] = regex.exec(decodedData);
+  const [match] = regex.exec(serializedDom);
   const { captionTracks } = JSON.parse(`${match}}`);
 
   const subtitle =
-    find(captionTracks, {
-      vssId: `.${lang}`,
-    }) ||
-    find(captionTracks, {
-      vssId: `a.${lang}`,
-    }) ||
-    find(captionTracks, ({ vssId }) => vssId && vssId.match(`.${lang}`));
+    captionTracks.find(cap => cap.vssId && cap.vssId === `.${lang}`) ||
+    captionTracks.find(cap => cap.vssId && cap.vssId === `a.${lang}`) ||
+    captionTracks.find(cap => cap.vssId && cap.vssId.match(`.${lang}`));
 
   // * ensure we have found the correct subtitle lang
   if (!subtitle || (subtitle && !subtitle.baseUrl))
     throw new Error(`Could not find ${lang} captions for ${videoID}`);
 
-  const { data: transcript } = await axios.get(
-    `https://www.youtube.com${subtitle.baseUrl}`
-  );
+  const xmlData = await fetch(`https://www.youtube.com${subtitle.baseUrl}`);
+  const transcript = await xmlData.text();
+
   const lines = transcript
     .replace('<?xml version="1.0" encoding="utf-8" ?><transcript>', '')
     .replace('</transcript>', '')
@@ -50,10 +44,10 @@ export async function getSubtitles({
     .filter(line => line && line.trim())
     .map(line => {
       const startRegex = /start="([\d.]+)"/;
-      const durRegex = /dur="([\d.]+)"/;
+      const durationRegex = /dur="([\d.]+)"/;
 
       const [, start] = startRegex.exec(line);
-      const [, dur] = durRegex.exec(line);
+      const [, duration] = durationRegex.exec(line);
 
       const htmlText = line
         .replace(/<text.+>/, '')
@@ -63,11 +57,7 @@ export async function getSubtitles({
       const decodedText = he.decode(htmlText);
       const text = striptags(decodedText);
 
-      return {
-        start,
-        dur,
-        text,
-      };
+      return { start, duration, text };
     });
 
   return lines;
--- test/index.test.js
+++ test/index.test.js
@@ -4,7 +4,7 @@ import { getSubtitles } from '../src';
 test('Extract estonia war subtitles', async t => {
   const subtitles = await getSubtitles({ videoID: 'HBA0xDHZjko' });
   t.deepEqual(subtitles[0], {
-    dur: '5.87',
+    duration: '5.87',
     start: '6.62',
     text: 'November 19',
   });
